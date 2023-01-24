c13800a651b32ef0fcc29ef7c155fa1a0458752c
simonbjerkas <simon.bjerkas10@gmail.com>
2023-01-24 19:19:41 +0100

    Refactoring sorter class with numberscollection

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -1,22 +1,17 @@
+import { NumbersCollection } from './NumbersCollection';
+
 export class Sorter {
-  constructor(public collection: /* TODO: add annotations */) {}
+  constructor(public collection: NumbersCollection) {}
 
   sort(): void {
     const { length } = this.collection;
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
-
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftHand = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftHand;
+        if (this.collection.compare(j, j + 1)) {
+          this.collection.swap(j, j + 1);
         }
       }
     }
   }
 }
-
-const sorter = new Sorter([10, 3, -5, 0]);
-sorter.sort();
-console.log(sorter.collection);
