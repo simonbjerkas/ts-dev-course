adf77103a861763f71d821516bdac6eeab93e5b3
simonbjerkas <simon.bjerkas10@gmail.com>
2023-01-24 19:45:25 +0100

    Updated index.ts to test CharactersCollection

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
+import { CharactersCollection } from './CharactersCollection';
 import { NumbersCollection } from './NumbersCollection';
 import { Sorter } from './Sorter';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+const charactersCollection = new CharactersCollection('Bac');
 
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+const numSorter = new Sorter(numbersCollection);
+const strSorter = new Sorter(charactersCollection);
+numSorter.sort();
+strSorter.sort();
+
 console.log(numbersCollection.data);
+console.log(charactersCollection.data);
