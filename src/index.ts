a286521102cc7a66892b68c1f4e9779ab72077ac
simonbjerkas <simon.bjerkas10@gmail.com>
2023-01-24 21:09:37 +0100

    Swapped the sortable interface with an abstarct sorter class

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 import { NumbersCollection } from './NumbersCollection';
-import { Sorter } from './Sorter';
 
-const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const charactersCollection = new CharactersCollection('Bac');
+const numbersCollection = new NumbersCollection([50, 3, -5, 0]);
+const charactersCollection = new CharactersCollection('Xaayb');
+const linkedList = new LinkedList();
 
-const numSorter = new Sorter(numbersCollection);
-const strSorter = new Sorter(charactersCollection);
-numSorter.sort();
-strSorter.sort();
+linkedList.add(500);
+linkedList.add(1200);
+linkedList.add(-80);
+linkedList.add(-10);
 
 console.log(numbersCollection.data);
 console.log(charactersCollection.data);
+linkedList.print();
+
+numbersCollection.sort();
+charactersCollection.sort();
+linkedList.sort();
+
+console.log(numbersCollection.data);
+console.log(charactersCollection.data);
+linkedList.print();
