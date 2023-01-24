cc3ba905634c012282b4c3c820aac4c66f677709
simonbjerkas <simon.bjerkas10@gmail.com>
2023-01-24 15:55:01 +0100

    bubble sort 1.0 implemented

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 class Sorter {
   constructor(public collection: number[]) {}
 
-  sort(): void {}
+  sort(): void {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const leftHand = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + i] = leftHand;
+        }
+      }
+    }
+  }
 }
 
-const rter = new Sorter([1, 2, 5, 4]);
+const sorter = new Sorter([1, 2, 5, 4]);
