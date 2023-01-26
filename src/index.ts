30deab6dbb548ee46940cd25f9c25f3355c8bea0
simonbjerkas <simon.bjerkas10@gmail.com>
2023-01-26 13:16:20 +0100

    save method added to User

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 import { User } from './models/User';
 
-const user = new User({ id: 1 });
+const user = new User({ name: 'newRecord', age: 0 });
 
-user.fetch();
-
-setTimeout(() => {
-  console.log(user);
-}, 3000);
+user.save();
