e05301d8501e925a43250de2c28594c96035848f
simonbjerkas <simon.bjerkas10@gmail.com>
2023-01-27 13:28:30 +0100

    Adding passthrough methods in user class

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
 import { User } from './models/User';
 
 const user = new User({ name: 'newRecord', age: 0 });
-
-user.events.on('change', () => {
-  console.log('Change!!!');
-});
-
-user.events.trigger('change');
