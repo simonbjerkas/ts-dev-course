36f4651e781581b29e713403ec277ebce116d62d
simonbjerkas <simon.bjerkas10@gmail.com>
2023-01-27 12:06:17 +0100

    fetch and save refactor

--- src/models/Sync.ts
+++ src/models/Sync.ts
@@ -1,23 +1,19 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosPromise } from 'axios';
 import { UserProps } from './User';
 
 export class Sync {
   constructor(public rootUrl: string) {}
 
-  fetch(): void {
-    axios
-      .get(`${this.rootUrl}/${this.get('id')}`)
-      .then((response: AxiosResponse): void => {
-        this.set(response.data);
-      });
+  fetch(id: number): AxiosPromise {
+    return axios.get(`${this.rootUrl}/${id}`);
   }
 
-  save(data: UserProps): void {
+  save(data: UserProps): AxiosPromise {
     const { id } = data;
     if (id) {
-      axios.put(`${this.rootUrl}/${id}`, data);
+      return axios.put(`${this.rootUrl}/${id}`, data);
     } else {
-      axios.post(this.rootUrl, data);
+      return axios.post(this.rootUrl, data);
     }
   }
 }
