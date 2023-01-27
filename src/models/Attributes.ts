974c94f1816a7968d1a55e4a3d6fd2dec261877e
simonbjerkas <simon.bjerkas10@gmail.com>
2023-01-27 12:58:31 +0100

    Advanced generics constraints in attributes get method

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -1,8 +1,8 @@
 export class Attributes<T extends Object> {
   constructor(private data: T) {}
 
-  get(propName: string): string | number {
-    return this.data[propName];
+  get<K extends keyof T>(key: K): T[K] {
+    return this.data[key];
   }
 
   set(update: T): void {
