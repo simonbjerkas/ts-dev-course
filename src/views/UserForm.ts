7a1b4dde7602a9949c59b18c3afc7fa01f6db736
simonbjerkas <simon.bjerkas10@gmail.com>
2023-01-30 13:39:32 +0100

    Stripped userform and added save function

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -6,6 +6,7 @@ export class UserForm extends View<User, UserProps> {
     return {
       'click:.set-age': this.onSetAgeClick,
       'click:.set-name': this.onSetNameClick,
+      'click:.save-model': this.onSaveClick,
     };
   }
 
@@ -21,15 +22,15 @@ export class UserForm extends View<User, UserProps> {
     }
   };
 
+  onSaveClick = (): void => this.model.save();
+
   template(): string {
     return `
     <div>
-      <h1>User form</h1>
-      <div>User name: ${this.model.get('name')}</div>
-      <div>User age: ${this.model.get('age')}</div>
-      <input />
+      <input placeholder="${this.model.get('name')}" />
       <button class="set-name">Update name</button>
       <button class="set-age">Set random age</button>
+      <button class="save-model">Save</button>
     </div>
     `;
   }
