1eba9e1e7894304ae0f4794bb1cd275aaa272923
simonbjerkas <simon.bjerkas10@gmail.com>
2023-01-30 12:50:00 +0100

    added tsconfig

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import { UserForm } from './views/UserForm';
 
 const user = User.buildUser({ name: 'Simon', age: 21 });
 
-const userForm = new UserForm(document.getElementById('root')!, user);
+const root = document.getElementById('root');
 
-userForm.render();
+if (root) {
+  const userForm = new UserForm(root, user);
+  userForm.render();
+} else throw new Error('Root element not found');
