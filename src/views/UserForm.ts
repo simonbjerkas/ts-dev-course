d34f9f6ab56f4b608a5cee702293d5755d86c53f
simonbjerkas <simon.bjerkas10@gmail.com>
2023-01-30 12:40:12 +0100

    Added feature to update name

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -12,11 +12,19 @@ export class UserForm {
   eventsMap(): { [key: string]: () => void } {
     return {
       'click:.set-age': this.onSetAgeClick,
+      'click:.set-name': this.onSetNameClick,
     };
   }
 
   onSetAgeClick = (): void => {
     this.model.setRandomAge();
+  };
+
+  onSetNameClick = (): void => {
+    const input = this.parent.querySelector('input');
+    const name = input?.value;
+
+    this.model.set({ name });
   };
 
   template(): string {
@@ -26,7 +34,7 @@ export class UserForm {
       <div>User name: ${this.model.get('name')}</div>
       <div>User age: ${this.model.get('age')}</div>
       <input />
-      <button>Click me</button>
+      <button class="set-name">Update name</button>
       <button class="set-age">Set random age</button>
     </div>
     `;
