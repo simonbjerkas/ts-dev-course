5916236bc5f12a33878a987e18b4ed9b8c031a55
simonbjerkas <simon.bjerkas10@gmail.com>
2023-01-30 16:54:23 +0100

    routes added with body parser

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
+import bodyParser from 'body-parser';
 import express, { Request, Response } from 'express';
 import { router } from './routes/loginRoutes';
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
 
 app.listen(3000, () => {
