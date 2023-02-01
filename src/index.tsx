0a7bde1d8776a5ec1a3f43aaa5f41ed20909fe9d
simonbjerkas <simon.bjerkas10@gmail.com>
2023-02-01 15:04:21 +0100

    const App instead of class

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
-class App extends React.Component {
-  render(): React.ReactNode {
-    return <div>Hi there</div>;
-  }
+interface AppProps {
+  color?: string;
 }
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+const App = (props: AppProps): JSX.Element => {
+  return <div>{props.color}</div>;
+};
+
+// class App extends React.Component<AppProps> {
+//   state = { counter: 0 };
+
+//   onIncrement = (): void => {
+//     this.setState({ counter: this.state.counter + 1 });
+//   };
+//   onDecrement = (): void => {
+//     this.setState({ counter: this.state.counter - 1 });
+//   };
+
+//   render(): React.ReactNode {
+//     return (
+//       <div>
+//         <button onClick={this.onIncrement}>Increment</button>
+//         <button onClick={this.onDecrement}>Decrement</button>
+//         {this.state.counter}
+//       </div>
+//     );
+//   }
+// }
+
+const container = document.getElementById('root');
+const root = createRoot(container!);
+
+root.render(<App color="red" />);
