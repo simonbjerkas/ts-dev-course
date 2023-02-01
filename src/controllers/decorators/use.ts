5af3eed7d14fa3965ad2d5588863d110a7f410ac
simonbjerkas <simon.bjerkas10@gmail.com>
2023-02-01 11:01:35 +0100

    added use decorator

---
+++ src/controllers/decorators/use.ts
@@ -0,0 +1,19 @@
+import { RequestHandler } from 'express';
+import 'reflect-metadata';
+import { MetadataKeys } from './MetadataKeys';
+
+export function use(middleware: RequestHandler) {
+  return function (target: any, key: string, desc: PropertyDescriptor): void {
+    const middlewares =
+      Reflect.getMetadata(MetadataKeys.middleware, target, key) || [];
+
+    middlewares.push(middleware);
+
+    Reflect.defineMetadata(
+      MetadataKeys.middleware,
+      [...middlewares, middleware],
+      target,
+      key
+    );
+  };
+}
